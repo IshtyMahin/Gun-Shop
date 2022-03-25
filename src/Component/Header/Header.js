fc6ff6571f580ecef7ef94c940779ab274ba5bdf
Ishtiaq uddin <ishty119@gmail.com>
2022-03-25 22:25:05 +0600

    header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <h1>Ishtiaq</h1>
         </div>
     );
 };
