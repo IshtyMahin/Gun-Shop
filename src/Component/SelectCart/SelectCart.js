5bbd7ffba17bdf61a7bd8c019acfd8459ec29017
Ishtiaq uddin <ishty119@gmail.com>
2022-03-25 22:17:37 +0600

    first commit

---
+++ src/Component/SelectCart/SelectCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectCart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SelectCart;
---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import SelectCart from '../SelectCart/SelectCart';
+
+const Shop = () => {
+    return (
+        <div>
+            <Cart></Cart>
+            <SelectCart></SelectCart>
+        </div>
+    );
+};
+
+export default Shop;
