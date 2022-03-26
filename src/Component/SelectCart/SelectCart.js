6d03d1a29b62d0d0e79285bf1605c831289ac689
Ishtiaq uddin <ishty119@gmail.com>
2022-03-26 13:41:34 +0600

    commit

--- src/Component/SelectCart/SelectCart.js
+++ src/Component/SelectCart/SelectCart.js
@@ -1,24 +1,26 @@
-import React from 'react';
-// import Choose from '../Choose/Choose';
-import "./SelectCart.css"
+import React from "react";
+
+import "./SelectCart.css";
 const SelectCart = (props) => {
-    const {cart,randomCart ,randomSelected , clearCart} =props
+  const { cart, randomCart, randomSelected, clearCart } = props;
 
+  return (
+    <div>
+      <div>
+        {cart.map((cart) => (
+          <h2>Name:{cart.name}</h2>
+        ))}
+      </div>
 
-    return (
-        <div>
-            
-            <div>
-            {
-                cart.map((cart) =><h2>Name:{cart.name}</h2>)
-            }
-            </div>
-            
-            <button className='select' onClick={()=>(randomSelected(cart))}>Select one</button>
-            <h1>{randomCart.name}</h1>
-            <button className='remove' onClick={clearCart}>remove all</button>
-        </div>
-    );
+      <button className="select" onClick={() => randomSelected(cart)}>
+        Select one
+      </button>
+      <h1>{randomCart.name}</h1>
+      <button className="remove" onClick={clearCart}>
+        remove all
+      </button>
+    </div>
+  );
 };
 
 export default SelectCart;
